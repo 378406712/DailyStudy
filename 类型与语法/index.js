97b94cca10173b8f86a8fdb1533f0f51707dfbfa
Clover <378406712@qq.com>
2020-10-05 03:44:57 +0800

    type

---
+++ 类型与语法/index.js
@@ -0,0 +1,16 @@
+// console.log(typeof null ==='object')
+// console.log(typeof function a (){} )
+function foo(a, b) {}
+console.log(foo.length)
+
+console.log(typeof 1)
+if (2 < 1) {
+  var a = 3
+}
+console.log(a)
+
+function bar() {
+  b = 3
+}
+bar()
+console.log(b)
