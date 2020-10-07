bd57886c891531806664c9deb34802b2bf8d177d
Clover <378406712@qq.com>
2020-10-07 22:31:47 +0800

    原生函数

---
+++ 类型与语法/count.js
@@ -0,0 +1,20 @@
+// // // var str = 'foo'
+// // // var newStr = str.concat('bar')
+// // // console.log(newStr)
+// // // var a = str.charAt(0)
+// // // console.log(a)
+
+// // // var c = Array.prototype.map
+// // //   .call(str, (v) => {
+// // //     return v.toUpperCase()
+// // //   })
+// // //   .join()
+// // // console.log(c)
+
+// // // var d = str.split('').reverse().join('')
+// // // console.log(d)
+
+// // var a = 5E10
+// // console.log(a.toExponential())
+// // console.log(42 .toFixed(3))
+// console.log(Object.is(+0 == -0))
